5af0c3a409fd1704483af9657440b55786aeea66
vifatala <vifatala@gmail.com>
2023-09-05 23:50:03 -0400

    Uploading Dijikstras Algorithm

---
+++ Graphs/Dijikstras Algorithm/dijkstras.py
@@ -0,0 +1,20 @@
+from math import inf
+from heapq import heappop, heappush
+
+def dijikstras(graph, start):
+  distances = {}
+  for vertex in graph:
+    distances[vertex] = inf
+  
+  distances[start] = 0
+  vertices_to_explore = [(0, start)]
+
+  while vertices_to_explore:
+    current_distance, current_vertex = heappop(vertices_to_explore)
+    for neighbor, edge_weight in graph[current_vertex]:
+      new_distance = current_distance + edge_weight
+      if new_distance < distances[neighbor]:
+        distances[neighbor] = new_distance
+        heappush(vertices_to_explore, (new_distance, neighbor))
+  
+  return distances
